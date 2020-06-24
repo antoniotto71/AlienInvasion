a28673a3d34b9e37bee4bbbc89051cb2d08fc384
antoniotto71 <antoniotto71@yahoo.it>
2020-06-24 19:57:50 +0200

    added voyager as exercise and a new bg color

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,16 +17,13 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
-        # Set the background color
-        self.bg_color = (230, 230, 230)
-
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self._update_screen()
 
-    def _check_events(self):
+    def _check_events(self):  # may be a static method
         """Respond to keypresses and mouse events"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
